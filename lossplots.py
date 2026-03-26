c5f0715f2de986ac98b063d5dfdbfbc4d583961e
Suyog Garg <gargsuyog@g.ecc.u-tokyo.ac.jp>
2026-03-26 16:57:45 +0900

    flexcvae-optuna: got training loss curve, not good. val-reconst-loss is darn constant, not learning. this is a classic case of posterior collapse

--- lossplots.py
+++ lossplots.py
@@ -56,8 +56,6 @@ def plot_running_loss(dir=DIR, time=TIME):
     plt.savefig(figname, dpi=300, bbox_inches='tight', transparent=True)
     plt.savefig(figname.replace('plot_', 'plot_white_'), dpi=300, bbox_inches='tight')
     plt.close()
-
-
   
     # dir = '../results/20250813/'
     # time = '20250813_033457-10'
@@ -86,6 +84,32 @@ def plot_running_loss(dir=DIR, time=TIME):
     # figname = dir + 'loss_plot_' + time + '.png'
     # plt.savefig(figname, dpi=300, bbox_inches='tight')
     # plt.show()
+
+
+def plot_flexcvae_loss(dir=DIR, time=TIME):
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+    df_train = pd.read_csv(dir + '/losses-flexcvae-train-' + time + '.csv', delimiter=',', header=0)
+    df_val = pd.read_csv(dir + '/losses-flexcvae-val-' + time + '.csv', delimiter=',', header=0)
+    for col in df_train.columns:
+        ax.plot(range(len(df_train[col])), df_train[col], label=col)
+    for col in df_val.columns:
+        ax.plot(range(len(df_val[col])), df_val[col], label=col)
+    ax.set_xlabel('Cumulative Steps', fontsize=12)
+    ax.set_ylabel('Running Loss', fontsize=12)
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_xlim(1, 10**4)
+    ax.xaxis.set_minor_locator(plt.LogLocator(base=10.0, subs=np.arange(1.0, 10.0) * 0.1, numticks=10))
+    ax.yaxis.set_minor_locator(plt.LogLocator(base=10.0, subs=np.arange(1.0, 10.0) * 0.1, numticks=10))
+    ax.tick_params(which='both', direction='in', top=True, right=True)
+    ax.legend()
+    plt.tight_layout()
+    figname = dir + 'flexcvae_loss_plot_' + time + '.png'
+    plt.savefig(figname, dpi=300, bbox_inches='tight', transparent=True)
+    plt.savefig(figname.replace('plot_', 'plot_white_'), dpi=300, bbox_inches='tight')
+    plt.show()
+    plt.close()
+
 
 
 def plot_mm_hist(dfmm, log=False, fontsize=15, labelsize=13, fname=''):
@@ -126,47 +150,47 @@ def plot_mm_hist(dfmm, log=False, fontsize=15, labelsize=13, fname=''):
 
 
 def plot_mmcontour_in_qchi_space(dfmm, fontsize=15, labelsize=13, fname=''):
-        """
-        Plot the mismatches in the mass ratio and chi_eff plane as contours.
-        """
-        logging.info("Plotting mismatches in the mass ratio and chi_eff plane.")
-        titles = ['Amplitude', 'Frequency', '$\\mathbf{h_{+}}$', '$\\mathbf{h_{\\times}}$']
+    """
+    Plot the mismatches in the mass ratio and chi_eff plane as contours.
+    """
+    logging.info("Plotting mismatches in the mass ratio and chi_eff plane.")
+    titles = ['Amplitude', 'Frequency', '$\\mathbf{h_{+}}$', '$\\mathbf{h_{\\times}}$']
 
-        # Create a grid of points
-        q = dfmm['mass_ratio'].values
-        chi = dfmm['chi_eff'].values
-        xi = np.linspace(min(q), max(q), 100)
-        yi = np.linspace(min(chi), max(chi), 100)
-        xi, yi = np.meshgrid(xi, yi)
+    # Create a grid of points
+    q = dfmm['mass_ratio'].values
+    chi = dfmm['chi_eff'].values
+    xi = np.linspace(min(q), max(q), 100)
+    yi = np.linspace(min(chi), max(chi), 100)
+    xi, yi = np.meshgrid(xi, yi)
 
-        # Interpolate mismatch values onto the grid
-        zi_amp = griddata((q, chi), dfmm['mismatch_amp'].values, (xi, yi), method='linear')
-        zi_freq = griddata((q, chi), dfmm['mismatch_freq'].values, (xi, yi), method='linear')
-        zi_hplus = griddata((q, chi), dfmm['mismatch_hplus'].values, (xi, yi), method='linear')
-        zi_hcross = griddata((q, chi), dfmm['mismatch_hcross'].values, (xi, yi), method='linear')
-        data = [zi_amp, zi_freq, zi_hplus, zi_hcross]
+    # Interpolate mismatch values onto the grid
+    zi_amp = griddata((q, chi), dfmm['mismatch_amp'].values, (xi, yi), method='linear')
+    zi_freq = griddata((q, chi), dfmm['mismatch_freq'].values, (xi, yi), method='linear')
+    zi_hplus = griddata((q, chi), dfmm['mismatch_hplus'].values, (xi, yi), method='linear')
+    zi_hcross = griddata((q, chi), dfmm['mismatch_hcross'].values, (xi, yi), method='linear')
+    data = [zi_amp, zi_freq, zi_hplus, zi_hcross]
 
-        # Plotting
-        fig, axes = plt.subplots(2, 2, figsize=(12, 10))
-        contour_levels = np.logspace(-6, 0, 13)
+    # Plotting
+    fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+    contour_levels = np.logspace(-6, 0, 13)
 
-        for i, ax in enumerate(axes.flat):
-            cs = ax.contourf(xi, yi, data[i], levels=contour_levels, norm=LogNorm(), cmap='viridis')
-            fig.colorbar(cs, ax=ax, label=f'{titles[i]} mismatch')
-            # ax.set_title(titles[i], fontsize=fontsize, fontweight='bold')
-            ax.set_xlabel('q', fontsize=fontsize)
-            ax.set_ylabel('$\\chi_{\\rm eff}$', fontsize=fontsize)
-            ax.minorticks_on()
-            ax.tick_params(which='both', direction='in', top=True, right=True)
-            ax.tick_params(labelsize=labelsize)
+    for i, ax in enumerate(axes.flat):
+        cs = ax.contourf(xi, yi, data[i], levels=contour_levels, norm=LogNorm(), cmap='viridis')
+        fig.colorbar(cs, ax=ax, label=f'{titles[i]} mismatch')
+        # ax.set_title(titles[i], fontsize=fontsize, fontweight='bold')
+        ax.set_xlabel('q', fontsize=fontsize)
+        ax.set_ylabel('$\\chi_{\\rm eff}$', fontsize=fontsize)
+        ax.minorticks_on()
+        ax.tick_params(which='both', direction='in', top=True, right=True)
+        ax.tick_params(labelsize=labelsize)
 
-        plt.tight_layout()
-        savename = DIR + f'mmcontour_in_qchi'
-        savename += fname if fname else ''
-        plt.savefig(savename+'-'+TIME+'.png', dpi=300, bbox_inches='tight', transparent=True)
-        plt.savefig(savename+'-white'+'-'+TIME+'.png', dpi=300, bbox_inches='tight')
-        logging.info(f"Mismatch in q-chi_eff plane plot saved to {savename}")
-        plt.close()
+    plt.tight_layout()
+    savename = DIR + f'mmcontour_in_qchi'
+    savename += fname if fname else ''
+    plt.savefig(savename+'-'+TIME+'.png', dpi=300, bbox_inches='tight', transparent=True)
+    plt.savefig(savename+'-white'+'-'+TIME+'.png', dpi=300, bbox_inches='tight')
+    logging.info(f"Mismatch in q-chi_eff plane plot saved to {savename}")
+    plt.close()
 
 
 def plot_mm_vs_mass(dfmm, fontsize=15, labelsize=13, fname=''):
@@ -381,10 +405,17 @@ if __name__ == "__main__":
                         help='Plot contour plots of mismatch values in the mass ratio and chi_eff plane.')
     parser.add_argument('--mm_in_qchi', action='store_true',
                         help='Plot mismatch values against mass parameters and chi_eff.')
+
+    parser.add_argument('--dir', type=str, default='../results/',
+                        help='Directory containing the results files. (default:{%(default)s})')
+    parser.add_argument('--time', type=str, default=datetime.now().strftime('%Y%m%d-%H%M%S'),
+                        help='Timestamp to identify the specific results files to use. (default:{%(default)s})')
+    
+    parser.add_argument('--verbose', '-v', action='store_true', help='Enable verbose logging for debugging purposes.')
     args = parser.parse_args()
 
     # # save log to a file
-    # log_filename = DIR + 'calc-mismatches-' + TIME + '.log'
+    # log_filename = args.dir + 'calc-mismatches-' + args.time + '.log'
     # file_handler = logging.FileHandler(log_filename)
     # file_handler.setLevel(logging.INFO)
     # formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -392,11 +423,13 @@ if __name__ == "__main__":
     # logging.getLogger().addHandler(file_handler)
 
 
-    # logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', 
-    #                     datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(level=logging.INFO if args.verbose == 0 else logging.DEBUG, 
+                        format='%(asctime)s - %(levelname)s - %(message)s', 
+                        datefmt='%Y-%m-%d %H:%M:%S')
 
 
-    # plot_running_loss()
+    # plot_running_loss(dir=args.dir, time=args.time)
     # mismatch_anal(args)
     # plot_rom_opt_mm_hist()
-    plot_timecompare_from_file()
+    # plot_timecompare_from_file()
+    plot_flexcvae_loss(dir=args.dir, time=args.time)
