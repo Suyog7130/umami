bcefa922c581a2e963370a653f729493e002d332
Suyog Garg <gargsuyog@g.ecc.u-tokyo.ac.jp>
2026-04-13 14:02:12 +0900

    test-time: FIX add some dummy warm-up

--- maincvae.py
+++ maincvae.py
@@ -1299,7 +1299,8 @@ class Test:
         # memory is being used, the time taken for N waveforms to generate
         # is mostly the same. Changes occur after GPU memory is filled-up!
         for _ in range(10):
-            labels = torch.tensor([[10, 10, -0.5, 0.5]], dtype=getattr(torch, self.precision)).to(self.device)
+            labels = torch.tensor([[10, 10, -0.5, 0.5],[10, 10, -0.5, 0.5]], 
+                                  dtype=getattr(torch, self.precision)).to(self.device)
             with torch.no_grad():
                 z1_mean, z1_log_var = model.encode_label_for_x(labels)
                 z1p_mean, z1p_log_var = model.encode_label_for_key(labels)
